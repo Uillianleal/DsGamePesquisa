885ba9d218328930643277b4b6f48f4525ff80eb
Uillian Leal <willianleal52@hotmail.com>
2023-05-30 10:55:17 -0300

    Project created

---
+++ backend/src/main/java/com/dev/dsPesquisaGame/DsPesquisaGameApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dsPesquisaGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPesquisaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPesquisaGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/dsPesquisaGame/DsPesquisaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dsPesquisaGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPesquisaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
